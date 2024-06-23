c290469455d4f01cd21f360b3d78664db1867f0f
unknown <parhomchik93@gmail.com>
2024-06-23 20:18:12 +0300

    component-life-cycle

---
+++ src/app/lifecycle/components/parent/parent.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-parent',
+    templateUrl: './parent.component.html',
+    styleUrl: './parent.component.scss'
+})
+export class ParentComponent {
+    
+    public obj: string = 'title'
+
+    public show = true
+
+    public handleEvent(value: string) {
+        this.obj = value
+    }
+}
