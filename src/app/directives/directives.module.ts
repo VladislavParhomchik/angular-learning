0091e4957be97c12658f558184b1350c4bd57a80
unknown <parhomchik93@gmail.com>
2024-06-23 18:49:46 +0300

    directives

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DirectivesPageComponent } from './directives.component';
 import { DirectivesRouting } from './directives-routing.module';
+import { MyChangeColorDirectiveDirective } from './directive/my-change-color-directive.directive';
+import { MyIfDirectiveDirective } from './directive/my-if-directive.directive';
 
 
 @NgModule({
     imports: [CommonModule, DirectivesRouting],
     exports: [],
-    declarations: [DirectivesPageComponent],
+    declarations: [DirectivesPageComponent, MyChangeColorDirectiveDirective, MyIfDirectiveDirective],
     providers: [],
 })
 export class DirectivesModule { }
